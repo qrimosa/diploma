f0c777a150d1c7925a43448c9864a0d5515526bf
Ivan <pataymolodoy@gmail.com>
2024-06-28 13:47:59 +0300

    pythonanywhere

--- InternetShop/InternetShop/settings.py
+++ InternetShop/InternetShop/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'InternetShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'netniche$default',
+        'USER': 'netniche',
+        'PASSWORD': 'n123123123',
+        'HOST': 'netniche.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
